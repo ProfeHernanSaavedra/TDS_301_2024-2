61115332f2b6c29354183c54bff1d1c509f44596
hernansaavedrag <hernan.saavedra@gmail.com>
2024-08-09 12:27:55 -0400

    ejemplos clase 09-08-2024

---
+++ EstructuraDatos/EstructuraDatos/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructuraDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] dias = new string[7];
+            string[] dias = { "lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo" };
+            Console.WriteLine(dias[0]+"\n");
+
+            //for+tab+tab
+            Console.WriteLine("Usando For");
+            for (int i = 0; i < dias.Length; i++)
+            {
+                Console.WriteLine(dias[i]);
+            }
+
+            Console.WriteLine("\nUsando For each");
+            foreach (string dia in dias)
+            {
+                Console.WriteLine(dia);
+            }
+
+            int[,] sala = new int[8, 10];
+            Random rand = new Random(); //instanciar la clase Random!
+            Console.WriteLine("\n\nMatriz de {0} x {1}",sala.GetLength(0),sala.GetLength(1));
+            Console.WriteLine("Contenido posición 0,0: "+ sala[0,0]);
+
+            Console.WriteLine("\nRecorrer con for");
+            for (int f = 0; f < sala.GetLength(0); f++)
+            {
+                for (int c = 0; c < sala.GetLength(1); c++)
+                {
+                    sala[f, c] = rand.Next(0, 2);
+                    Console.Write(sala[f,c] + " ");
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("\n\nListas");
+            List<string> colores = new List<string>();
+            colores.Add("rojo");
+            colores.Add("azul");
+            colores.Add("verde");
+
+            Console.WriteLine("Primer elemento: " + colores[0]);
+
+            Console.WriteLine("\nRecorrido con foreach");
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            colores.Add("amarillo");
+            Console.WriteLine("\nRecorrido con foreach");
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            colores.Insert(0, "blanco");
+
+            Console.WriteLine("\nRecorrido con foreach");
+            foreach (string color in colores)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.WriteLine("Total de elementos de la lista: " + colores.Count);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MiPrimerApp/MiPrimerApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            //cw+tab+tab
+            int num1 = 1, num2 = 5;
+            int suma,num6;
+            string nombre = "Juan";
+            bool esCSharp = true;
+            float num3 = 3.1f;
+            decimal num4 = 4.3m;
+            string num5 = "14";
+
+            Console.WriteLine("Hola " + nombre) ;
+            Console.WriteLine("Lenguaje C# : " + esCSharp + "\n");
+
+            Console.WriteLine("Ingrese un número: ");
+            //num6 = int.Parse(Console.ReadLine());
+            int.TryParse(Console.ReadLine(), out num6);
+            suma = num1 + num2 + (int)num3 + (int)num4 + int.Parse(num5) + num6;
+            Console.WriteLine(suma);
+            //Esto es para que no se cierre la consola
+            Console.ReadLine();
+
+            
+        }
+    }
+}
