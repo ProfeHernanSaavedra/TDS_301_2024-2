de66466ca2ddaf817b4f4408e68bc46cb01cd177
hernansaavedrag <hernan.saavedra@gmail.com>
2024-08-21 13:00:38 -0400

    POO clase 21-08-2024

---
+++ Contactos/PresentacionWpf/Forms/UcNuevoContacto.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Logica;
+
+namespace PresentacionWpf.Forms
+{
+    /// <summary>
+    /// Lógica de interacción para UcNuevoContacto.xaml
+    /// </summary>
+    public partial class UcNuevoContacto : UserControl
+    {
+        public UcNuevoContacto()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DtFechaNacimiento.DisplayDateEnd = DateTime.Today;
+            DtFechaNacimiento.SelectedDate = DateTime.Today;
+
+            CboGrupo.ItemsSource = Enum.GetValues(typeof(Clasificacion));
+            CboGrupo.SelectedItem = Clasificacion.Default;
+        }
+    }
+}
